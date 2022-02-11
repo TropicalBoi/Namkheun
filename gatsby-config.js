df50187e6d9cabf3941acf61c1e183fd4bff959a
TropicalBoi <tewprai.bualoi@gmail.com>
2022-02-11 20:56:55 +0700

    :hammer:fix cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,16 +28,6 @@ module.exports = {
       },
       __key: "pages"
     }, {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "SWAPI",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "swapi",
-        // Url to query from
-        url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
-      },
-    }, {
       resolve: 'gatsby-plugin-local-search',
       options: {
         name: 'pages',
